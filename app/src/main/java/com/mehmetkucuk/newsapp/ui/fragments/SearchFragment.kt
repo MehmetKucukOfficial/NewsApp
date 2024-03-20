5dab1b7e061cbcb902d4cd413a104b2956f2e170
Mehmet Küçük <mehmetkucuk3599@gmail.com>
2024-03-21 01:31:47 +0300

    RetrofitInstance was added.

--- app/src/main/java/com/mehmetkucuk/newsapp/ui/fragments/SearchFragment.kt
+++ app/src/main/java/com/mehmetkucuk/newsapp/ui/fragments/SearchFragment.kt
@@ -8,7 +8,7 @@ import android.view.ViewGroup
 import com.example.newsprojectpractice.R
 
 
-class FavouritesFragment : Fragment() {
+class SearchFragment : Fragment() {
 
 
     override fun onCreateView(
@@ -16,7 +16,7 @@ class FavouritesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favourites, container, false)
+        return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
 
